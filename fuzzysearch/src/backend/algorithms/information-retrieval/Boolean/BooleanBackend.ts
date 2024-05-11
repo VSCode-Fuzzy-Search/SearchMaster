d659011599d46ac87ce2c743d4a8d0f0ed3c2ffc
James Hunt <jhun0012@student.monash.edu>
2024-05-11 22:17:20 +1000

    Added testing file and mock BooleanBackend

--- fuzzysearch/src/backend/algorithms/information-retrieval/Boolean/BooleanBackend.ts
+++ fuzzysearch/src/backend/algorithms/information-retrieval/Boolean/BooleanBackend.ts
@@ -11,6 +11,10 @@ export default class BooleanBackend extends QueryBackend {
      */
     protected generateIndex(documents: Document[]): void {
         // TODO: implement this.
+        this.index = { "Hello": [documents[0].filename, documents[1].filename, documents[2].filename],
+        "Bye": [documents[1].filename, documents[2].filename],
+        "Goodbye": [documents[0].filename]
+        }
     }
 
     /**
@@ -29,6 +33,32 @@ export default class BooleanBackend extends QueryBackend {
     public handle(query: Query): QueryResponse {
         // TODO: implement this.
         const response: QueryResponse = {results: []};
+
+        let processedQuery = query.getFormattedQuery();
+        let andArray = []
+
+        for (let i = 0; i < processedQuery.length; i++){
+
+            if (processedQuery[i] == "AND"){
+
+                let arr1 = (this.index as {[word: string]: string[]})[processedQuery[i - 1]]
+                let arr2 = (this.index as {[word: string]: string[]})[processedQuery[i + 1]]
+                
+
+                for (let j = 0; j < arr2.length; j++){
+                    if (arr1.includes(arr2[j])){
+                        andArray.push(arr2[j])
+                    }
+                }
+
+            }
+
+        }
+
+        for (let i = 0; i < andArray.length; i++){
+            response.results.push({documentID: andArray[i]})
+        }
+        
         return response;
     }
 
