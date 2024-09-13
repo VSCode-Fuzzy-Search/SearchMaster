010c63dbd299cfc656e66a3e9a9d8fa45474cf78
Mason Hunt <masonhunt130ii@gmail.com>
2024-09-13 15:15:27 +1000

    DevOps test commit: No Tag

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,13 @@
 import { commands, ExtensionContext, window } from 'vscode';
 import { registerCacheCommand } from './extension/features/cache-operation';
-import { registerWebViewProvider } from "./extension/views/register-webview-provider";
+import { registerWebViewProvider } from './extension/views/register-webview-provider';
 
 export function activate(context: ExtensionContext) {
 	const op = window.createOutputChannel('Search Master');
 	registerCacheCommand(context);
 	registerWebViewProvider(context, op);
 	commands.executeCommand('setContext', 'isPrintContextMenu', true);
+	console.log("I'm sorry all I gotta test the devops lmao");
 }
 
-export function deactivate() { }
+export function deactivate() {}
