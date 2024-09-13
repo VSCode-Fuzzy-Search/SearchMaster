be946a6a1354f9cf2413cc8e0e457d50423f209b
Mason Hunt <masonhunt130ii@gmail.com>
2024-09-13 15:35:14 +1000

    DevOps test commit: No Tag (v2 electric boogaloo)

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ export function activate(context: ExtensionContext) {
 	registerCacheCommand(context);
 	registerWebViewProvider(context, op);
 	commands.executeCommand('setContext', 'isPrintContextMenu', true);
-	console.log("I'm sorry all I gotta test the devops lmao");
+	console.log("I'm sorry all I gotta test the devops :/");
 }
 
 export function deactivate() {}
