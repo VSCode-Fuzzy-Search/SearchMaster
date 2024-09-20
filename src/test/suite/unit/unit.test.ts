90f1cbccaa04cb276ae018123f4facd8c39dd2be
Alex Kanellis <akan0033@student.monash.edu>
2024-09-20 15:46:51 +1000

    writing unit tests

--- src/test/suite/unit/unit.test.ts
+++ src/test/suite/unit/unit.test.ts
@@ -9,6 +9,13 @@
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
+import BackendFactory from '../../../extension/backend/algorithms/information-retrieval/BackendFactory';
+import { AlgorithmEnum } from '../../../extension/backend/AlgorithmEnum';
+import Query from '../../../extension/backend/queries/Query';
+import QueryFactory from '../../../extension/backend/queries/QueryFactory';
+import QueryBackend from '../../../extension/backend/algorithms/information-retrieval/QueryBackend';
+import QueryResponse from '../../../extension/backend/results/QueryResponse';
+
 // import * as myExtension from '../../extension';
 
 // Name of the test suite. Also used as a prefix for the test messages.
@@ -16,6 +23,24 @@ const SUITE_NAME = 'Unit';
 
 // Extends objects with a `should` property for chaining assertions.
 var should = require('chai').should();
+
+
+//functions to testing backend algorithms
+function searchBackend(algorithm: AlgorithmEnum, query: string): QueryResponse | undefined{
+	const backendFactory = new BackendFactory();
+	const backend = backendFactory.getBackend(algorithm);
+
+	const queryFactory = new QueryFactory();
+	const queryObject = queryFactory.createQuery(
+		query,
+		algorithm
+	);
+
+	let result = queryObject && backend?.handle(queryObject);
+	return result
+}
+
+
 
 suite(`${SUITE_NAME} Test Suite`, () => {
 	// Show a message before running the tests.
@@ -27,11 +52,37 @@ suite(`${SUITE_NAME} Test Suite`, () => {
 		done();
 	});
 
+
 	// Example test using Chai assertion library.
 	// TODO: Replace with real tests!
 	test('Example Test', () => {
 		let a = 1;
 		a.should.be.equal(1);
 		a.should.be.a('number');
+	});
+
+	test('Boolean Backend', () => {
+		//testing backend for boolean retrieval
+
+		//Arrange
+		const backendFactory = new BackendFactory();
+		const backend = backendFactory.getBackend(AlgorithmEnum.Boolean);
+		const queryFactory = new QueryFactory();
+
+		let booleanQuery = queryFactory.createQuery(
+			"hello world",
+			AlgorithmEnum.Boolean
+		);
+
+
+		//Act
+		const result = booleanQuery && backend?.handle(booleanQuery);
+
+
+		//Assert
+		result?.should.be.a('object');
+		result?.should.have.property('results');
+		result?.results.should.be.a('array');
+		result?.results.should.have.length.above(0);
 	});
 });
