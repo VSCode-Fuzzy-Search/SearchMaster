281f3d68f2e194100200d87450230f3c7ef32648
jackwigney <jack.wigney@gmail.com>
2024-10-04 09:51:35 +1000

    Converted QueryFactory to singleton instance

--- src/extension/backend/queries/QueryFactory.ts
+++ src/extension/backend/queries/QueryFactory.ts
@@ -6,6 +6,7 @@ import VectorQuery from "./VectorQuery";
 
 // potentially not an ideal approach, but can live with it for now - James
 export default class QueryFactory {
+    static instance: QueryFactory; // Singleton
     
     /**
      * Creates all query backends
@@ -13,6 +14,14 @@ export default class QueryFactory {
      * @param queryType enum of which algoirhtm to use for query
      * @returns select Query
      */
+
+    public static getInstance(): QueryFactory {
+        if (!QueryFactory.instance) {
+            QueryFactory.instance = new QueryFactory();
+        }
+        return QueryFactory.instance;
+    }
+
     public createQuery(query: string, queryType: AlgorithmEnum){
 
         if (queryType == AlgorithmEnum.Boolean){
