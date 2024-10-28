bfa161ee6fe4e28f8543f404dbf8eaba3dd8bc15
James Hunt <jhun0012@student.monash.edu>
2024-10-28 16:45:59 +1100

    Removing Unused Attribute

--- src/extension/backend/results/QueryResponse.ts
+++ src/extension/backend/results/QueryResponse.ts
@@ -1,6 +1,5 @@
 import QueryResult from "./QueryResult";
 export default interface QueryResponse {
-    readonly duration?: number; // in ms. How long the algorithm took
     readonly matchCount?: number; // number of times the query was found in the colleciton
     readonly corpusSize?: number; // number of documents in the collection that was looked through
     readonly results: QueryResult[] // list of the matched documents
