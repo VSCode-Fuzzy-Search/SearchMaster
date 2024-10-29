50edc28e241ee5f7e2d4c136f2b771f740221f8e
Brittank88 <masonhunt130ii@gmail.com>
2024-10-30 08:44:02 +1100

    Potential workaround for testing errors on Windows

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -5,7 +5,14 @@ import * as path from 'path';
 export function run(): Promise<void> {
 	// Create the mocha test runner.
 	const MOCHA = new Mocha({ ui: 'tdd', color: true });
-	const TESTS_ROOT = path.resolve(__dirname, '..');
+	var TESTS_ROOT = path.resolve(__dirname, '..');
+
+	// Workaround for https://github.com/microsoft/vscode-test/issues/134
+	// See also https://github.com/mochajs/mocha/issues/4851
+	// Suggested by https://github.com/microsoft/vscode-test-cli/issues/49#issue-2381284091
+	if (process.platform === 'win32') {
+		TESTS_ROOT = TESTS_ROOT.toLowerCase();
+	}
 
 	return new Promise((resolve, reject) => {
 		glob('**/**.test.js', { cwd: TESTS_ROOT }).then(files => {
