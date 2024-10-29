00890ff07038d41b82b49127c9489198e4880fe5
Brittank88 <masonhunt130ii@gmail.com>
2024-10-30 08:59:40 +1100

    Fix Mocha error on Windows

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,18 +1,11 @@
 import { glob } from 'glob';
 import * as Mocha from 'mocha';
-import * as path from 'path';
+import * as path from 'node:path';
 
 export function run(): Promise<void> {
 	// Create the mocha test runner.
 	const MOCHA = new Mocha({ ui: 'tdd', color: true });
-	var TESTS_ROOT = path.resolve(__dirname, '..');
-
-	// Workaround for https://github.com/microsoft/vscode-test/issues/134
-	// See also https://github.com/mochajs/mocha/issues/4851
-	// Suggested by https://github.com/microsoft/vscode-test-cli/issues/49#issue-2381284091
-	if (process.platform === 'win32') {
-		TESTS_ROOT = TESTS_ROOT.toLowerCase();
-	}
+	const TESTS_ROOT = path.resolve(__dirname, '..');
 
 	return new Promise((resolve, reject) => {
 		glob('**/**.test.js', { cwd: TESTS_ROOT }).then(files => {
