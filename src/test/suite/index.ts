8259adf495c284327e7b61a6b3220fa4576f3039
Brittank88 <masonhunt130ii@gmail.com>
2024-10-30 08:21:36 +1100

    Potential fix for Windows bug with Mocha
    See: https://github.com/mochajs/mocha/issues/4851#issuecomment-1086743320

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -3,20 +3,19 @@ import * as Mocha from 'mocha';
 import * as path from 'path';
 
 export function run(): Promise<void> {
-
 	// Create the mocha test runner.
-	const MOCHA = new Mocha({ ui: 'tdd', color: true });
+	const MOCHA = new Mocha({ ui: 'bdd', color: true });
 	const TESTS_ROOT = path.resolve(__dirname, '..');
 
 	return new Promise((resolve, reject) => {
 		glob('**/**.test.js', { cwd: TESTS_ROOT }).then(files => {
-
 			// Add files to the test suite
 			files.forEach(f => MOCHA.addFile(path.resolve(TESTS_ROOT, f)));
 
 			// Run the mocha tests.
-			try { MOCHA.run(failures => failures ? reject(new Error(`${failures} tests failed.`)) : resolve()); }
-			catch (err) {
+			try {
+				MOCHA.run(failures => (failures ? reject(new Error(`${failures} tests failed.`)) : resolve()));
+			} catch (err) {
 				console.error(err);
 				reject(err);
 			}
