9c93e39d28326d48138c83808060b3eda919657c
Brittank88 <masonhunt130ii@gmail.com>
2024-10-30 15:41:30 +1100

    Fix test-results not being generated

--- .mocha-multi-reporter.js
+++ .mocha-multi-reporter.js
@@ -3,8 +3,8 @@ const { reporters } = require('mocha');
 module.exports = class MultiReporter extends reporters.Base {
 	constructor(runner, options) {
 		super(runner, options, [
-			new reporters.Spec(runner, { reporterOptions: options.reporterOption?.specReporterOptions }),
-			new reporters.JSON(runner, { reporterOptions: options.reporterOption?.jsonReporterOptions })
+			new reporters.Spec(runner, { reporterOption: options.reporterOption?.specReporterOptions }),
+			new reporters.JSON(runner, { reporterOption: options.reporterOption?.jsonReporterOptions })
 		]);
 	}
 };
